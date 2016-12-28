e18d7541693bf659ad91974c80ee313964382b98
Martin Waby Smoger <mvsmoger@gmail.com>
2016-12-28 10:02:07 -0500

    1st Version
    Adding the 1st working version of the library to be used. Will be developed more later in the season.

---
+++ CrevoLib2/src/Robot.cpp
@@ -0,0 +1,42 @@
+#include "IterativeRobot.h"
+#include "OI.h"
+#include "Joystick.h"
+#include "DriveTrain.h"
+#include "CrevoRobot.h"
+#include "RobotDrive.h"
+
+class Robot: public IterativeRobot
+{
+private:
+
+	void RobotInit()
+	{
+
+	}
+	void AutonomousInit()
+	{
+
+	}
+
+	void AutonomousPeriodic()
+	{
+
+	}
+
+	void TeleopInit()
+	{
+
+	}
+
+	void TeleopPeriodic()
+	{
+
+	}
+
+	void TestPeriodic()
+	{
+
+	}
+};
+
+START_ROBOT_CLASS(Robot);
