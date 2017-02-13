3830770baef9fcc58fcefe7181a6c32fd6455f74
Martin Waby Smoger <mvsmoger@gmail.com>
2017-02-12 20:23:12 -0500

    FeedBack Class added
    created a new class that organizes sensor feedback that the driver or
    operator will receive.

---
+++ CrevoLib/src/FeedBack.cpp
@@ -0,0 +1,18 @@
+/*
+ * FeedBack.cpp
+ *
+ *  Created on: Feb 12, 2017
+ *      Author: Martin Smoger
+ */
+
+#include <FeedBack.h>
+
+FeedBack::FeedBack() {
+	// TODO Auto-generated constructor stub
+
+}
+
+FeedBack::~FeedBack() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ CrevoLib/src/FeedBack.h
@@ -0,0 +1,17 @@
+/*
+ * FeedBack.h
+ *
+ *  Created on: Feb 12, 2017
+ *      Author: Martin Smoger
+ */
+
+#ifndef SRC_FEEDBACK_H_
+#define SRC_FEEDBACK_H_
+
+class FeedBack {
+public:
+	FeedBack();
+	virtual ~FeedBack();
+};
+
+#endif /* SRC_FEEDBACK_H_ */
