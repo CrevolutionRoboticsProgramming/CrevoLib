5d83e2909db6bed262541b9905d6ede974260080
Martin Waby Smoger <mvsmoger@gmail.com>
2017-02-15 10:19:48 -0500

    Added new Auton Vector class.
    This class is for storing and controlling auton data and actions that
    will be used later

---
+++ CrevoLib/src/AutonVectors.cpp
@@ -0,0 +1,18 @@
+/*
+ * AutonVectors.cpp
+ *
+ *  Created on: Feb 15, 2017
+ *      Author: Martin
+ */
+
+#include <AutonVectors.h>
+
+AutonVectors::AutonVectors() {
+	// TODO Auto-generated constructor stub
+
+}
+
+AutonVectors::~AutonVectors() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ CrevoLib/src/AutonVectors.h
@@ -0,0 +1,17 @@
+/*
+ * AutonVectors.h
+ *
+ *  Created on: Feb 15, 2017
+ *      Author: Martin
+ */
+
+#ifndef SRC_AUTONVECTORS_H_
+#define SRC_AUTONVECTORS_H_
+
+class AutonVectors {
+public:
+	AutonVectors();
+	virtual ~AutonVectors();
+};
+
+#endif /* SRC_AUTONVECTORS_H_ */
