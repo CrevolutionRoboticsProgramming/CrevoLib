89c09277b3c39b564804c649f502acbd3cb8fc04
Martin Waby Smoger <mvsmoger@gmail.com>
2017-02-19 14:38:39 -0500

    Trying to readded deleted code

--- CrevoLib/src/AutonVectors.h
+++ CrevoLib/src/AutonVectors.h
@@ -8,6 +8,7 @@
 #ifndef SRC_AUTONVECTORS_H_
 #define SRC_AUTONVECTORS_H_
 
+#include <crevoglb.h>
 #include <CrevoRobot.h>
 #include <DriveTrain.h>
 #include <Math.h>
@@ -15,13 +16,9 @@
 class AutonVectors {
 public:
 
-	enum AutonSide { RED, BLUE};
-
 	enum AutonStratagey { SHOOT_FROM_HOPPER, SCORE_GEAR_RIGHT_SHOOT_FROM_LIFT, SCORE_GEAR_CENTER_SHOOT_FROM_BASELINE, SCORE_GEAR_LEFT_SHOOT_FROM_BASELINE, SCORE_GEAR_LEFT_KNOCK_DOWN_HOPPER };
 
 	enum AutonAction { SHOOT, MOVE, SCORE_GEAR, DUMP_HOPPER };
-
-	enum MotorState { ON, OFF };
 
 	void AutonSwitch(AutonStratagey strat);
 
