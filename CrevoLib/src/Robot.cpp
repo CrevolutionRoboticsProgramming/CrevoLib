30e8c568ad448f9c429173a5d08df67570a9e091
Martin Waby Smoger <mvsmoger@gmail.com>
2017-03-05 13:05:37 -0500

    Shooter power changes and shooter logic added.

--- CrevoLib/src/Robot.cpp
+++ CrevoLib/src/Robot.cpp
@@ -24,23 +24,6 @@ class Robot: public IterativeRobot, public OI, public DriveTrain
 {
 public:
 
-	/*/
-	 * 1. Shoot from hopper
-	 * 2. Score gear right shoot from lift
-	 * 3. Score gear center, shoot from base line
-	 * 4. Score gear left, shooter from baseline
-	 * 5. Score gear left, knock down hoppers
-	 * 6. Hopper knock down
-	 /*/
-
-	//Put AutonNames here
-	enum Autons{AutonMove, ForwardAndBackwards, EchyMemes, InternalScreams, VisionProcessingData};
-
-	int AutonChooser;
-	double speedShoot;
-	bool tankTrue = false;
-	bool visionDebug;
-
 /*----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 	void RobotInit() override {
 
@@ -49,17 +32,22 @@ public:
 		runTime            = new Timer();
 		fdbk.elapsedRumble = new Timer();
 
-
 		crvbot.robotInit();
+
 		/*/
 		 * Calibrates Gyro, needs two seconds in order to calibrate correctly.
 		/*/
 		crvbot.gyro->Calibrate();
-		Wait(1);
+
+		Wait(2);
+
 		/*/
 		 *	Command to start up the stream from the USB camera.
 		/*/
 		vs.startStream();
+		/*/
+		 *  This is setting up the network table to communicate preferences from smart dashboard to the RoboRIO
+		/*/
 		prefs = Preferences::GetInstance();
 
 		std::cout << "_____________________________________________" << std::endl;
@@ -80,18 +68,34 @@ public:
 	 * SendableChooser make sure to add them to the chooser code above as well.
 	 */
 /*----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+	/*/
+	 * 1. Shoot from hopper
+	 * 2. Score gear right shoot from lift
+	 * 3. Score gear center, shoot from base line
+	 * 4. Score gear left, shooter from baseline
+	 * 5. Score gear left, knock down hoppers
+	 * 6. Hopper knock down
+	 /*/
+	int AutonChooser;
+
+	enum Autons{AutonMove, ForwardAndBackwards, EchyMemes, InternalScreams, VisionProcessingData};
+
 	void AutonomousInit() override {
+
 		/*/
 		 *	Temporary: select what Auton you like to by its name. Will later be selected through the SmartDashboard.
 		/*/
+		AutonChooser = Autons::AutonMove;
+//		/*/
+//		 * Initializes the robots settings into the DriveTrain class to use its functions.
+//		/*/
+		crvbot.leftEnc->Reset();
+		crvbot.rightEnc->Reset();
 
-		AutonChooser = Autons::InternalScreams;
-		/*/
-		 * Initializes the robots settings into the DriveTrain class to use its functions.
-		/*/
 		initDrive(crvbot.robotDrive);
 
 		updateRobotStatus();
+
 		updateRobotPreference();
 
 		crvbot.gyro->Reset();
@@ -102,8 +106,11 @@ public:
 		std::cout << "" << std::endl;
 		std::cout << "|| Crevobot || In Autonomous Periodic Mode" << std::endl;
 		std::cout << "_____________________________________________" << std::endl;
+		hasReached = false;
 	}
 /*----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+	bool hasReached;
+	int BoilerPostition;
 	void AutonomousPeriodic() {
 
 		runTime->Start();
@@ -111,48 +118,57 @@ public:
 		while(IsAutonomous() && IsEnabled())
 		{
 			updateRobotStatus();
-			switch(AutonChooser)
-			{
-			case AutonMove:
-			{
-				driveByTime(2, 0.5, Forward);
-				Wait(2);
-				break;
-			}
-			case ForwardAndBackwards:
-			{
-				driveByTime(2, 0.2, Forward);
-				Wait(2);
-				driveByTime(2, 0.8 , Reverse);
-				Wait(2);
-				break;
-			}
-			case VisionProcessingData:
-			{
-				while(true) { vs.distanceFromBoiler(); }
-				break;
-			}
-			case EchyMemes:
-			{
-				break;
-			}
-			case InternalScreams:
-			{
-				moveMotor(0.5, 2, Forward);
-			}
-			case 9:
-			{
-				moveMotor(0.5, 2, Reverse);
-				break;
-			}
-			default:
-			{
-				crvbot.robotDrive->StopMotor();
-				crvbot.fuelManipulator->StopMotor();
-				crvbot.intakeRoller->StopMotor();
-				crvbot.hangerMotor->StopMotor();
-			}
-			}
+//			switch(AutonChooser)
+//			{
+//			case AutonMove:
+//			{
+				//driveByTime(2, 0.5, Reverse);
+				//Wait(2);
+				//break;
+			//}
+//			case ForwardAndBackwards:
+//			{
+//				driveByTime(2, 0.2, Forward);
+//				Wait(2);
+//				driveByTime(2, 0.8 , Reverse);
+//				Wait(2);
+//				break;
+//			}
+//			case VisionProcessingData:
+//			{
+//				while(true) { vs.distanceFromBoiler(); }
+//				break;
+//			}
+//			case EchyMemes:
+//			{
+				//driveByTime(2, 0.5, Reverse);
+//				while(vs.alinementToBoiler() <=  200  && !hasReached)
+//				{
+//					moveRobot(0.15, -0.15);
+//				}
+//				hasReached = true;
+//				moveRobot(0,0);
+
+//
+//				break;
+//			}
+//			case InternalScreams:
+//			{
+//				moveMotor(0.5, 2, Forward);
+//			}
+//			case 9:
+//			{
+//				moveMotor(0.5, 2, Reverse);
+//				break;
+//			}
+//			default:
+//			{
+//				crvbot.robotDrive->StopMotor();
+//				crvbot.fuelManipulator->StopMotor();
+//				crvbot.intakeRoller->StopMotor();
+//				crvbot.hangerMotor->StopMotor();
+//			}
+			//}
 		}
 
 		runTime->Stop();
@@ -164,6 +180,11 @@ public:
 		updateRobotPreference();
 
 		runTime->Reset();
+
+		crvbot.gyro->Reset();
+
+		crvbot.leftEnc->Reset();
+		crvbot.rightEnc->Reset();
 
 		std::cout << "_____________________________________________" << std::endl;
 		std::cout << "" << std::endl;
@@ -212,34 +233,38 @@ private:
 	Preferences *prefs;
 	Timer *runTime;
 
+
+	bool reverseDirection = false;
+
 	void DriveCode(void) {
 
 	     double Left_Y  = controllerJoystick(driverGamepad, 	   	 Axes::LEFT_Y);
 	     double Right_Y = controllerJoystick(driverGamepad, 		 Axes::RIGHT_Y);
 		 double Right_X = (0.65*controllerJoystick(driverGamepad, Axes::RIGHT_X));
 
-		 if(controllerButton(driverGamepad, Button::A))  tankTrue = true;
-		 if(controllerButton(driverGamepad, Button::B))  tankTrue = false;
+		 if(controllerButton(driverGamepad, Button::A))  reverseDirection = true;
+		 if(controllerButton(driverGamepad, Button::B))  reverseDirection = false;
 
 		 /*_________ Sets DriverJoystick in Tank Drive orientation _________*/
-		 if(tankTrue) crvbot.robotDrive->SetLeftRightMotorOutputs(LEFT_MULTIPLER*Left_Y, RIGHT_MULTIPLER*Right_Y);
+		 if(!reverseDirection)
+			 crvbot.robotDrive->SetLeftRightMotorOutputs((LEFT_MULTIPLER*(Left_Y - Right_X)), (RIGHT_MULTIPLER*(Left_Y + Right_X)));
 		 /*_________ Sets DriverJoystick in FirstPerosnDrive orientation _________*/
-	     else         crvbot.robotDrive->SetLeftRightMotorOutputs((LEFT_MULTIPLER*(Left_Y - Right_X)), (RIGHT_MULTIPLER*(Left_Y + Right_X)));
+	     else
+	    	 crvbot.robotDrive->SetLeftRightMotorOutputs((LEFT_MULTIPLER*(-Left_Y - Right_X)), (RIGHT_MULTIPLER*(-Left_Y + Right_X)));
 	}
 
+
+	bool shooterEnabled;
 	void ShootProcesses(void) {
 
-		if(toggleAction((operatorGamepad->GetRawAxis(3) > 0), crvbot.fuelManipulator, 0.78))
-		{
-			whilePressedAction(controllerButton(operatorGamepad, Button::LeftBumber), controllerButton(operatorGamepad, Button::RightBumber), crvbot.agitatorMotor, 0.6);
-		}
+		toggleAction((operatorGamepad->GetRawAxis(3) > 0), crvbot.fuelManipulator, 1);
+		whilePressedAction(controllerButton(operatorGamepad, Button::LeftBumber), controllerButton(operatorGamepad, Button::RightBumber), crvbot.agitatorMotor, 0.4);
 	}
 
 	void updateRobotStatus(void) {
 
 		SmartDashboard::PutNumber(" Total Runtime: ",        		  runTime->Get());
-		SmartDashboard::PutNumber(" SpeedShooter: ", 		 		  speedShoot);
-		SmartDashboard::PutBoolean(" TankDrive: ", 			 	  	  tankTrue);
+		SmartDashboard::PutBoolean(" ReverseDirection : ", 			  reverseDirection);
 		SmartDashboard::PutNumber(" LeftMotor Current: ",   		  crvbot.leftFrontMotor->GetOutputCurrent());
 		SmartDashboard::PutNumber(" RightMotor Current: ",  		  crvbot.rightFrontMotor->GetOutputCurrent());
 		SmartDashboard::PutNumber(" LeftMotor Voltage: ",   		  crvbot.leftFrontMotor->GetOutputVoltage());
@@ -251,22 +276,24 @@ private:
 		SmartDashboard::PutNumber(" Agitator Current: ",              crvbot.agitatorMotor->GetOutputCurrent());
 		SmartDashboard::PutNumber(" Left Side Encoder Count: ", 	  crvbot.leftEnc->GetRaw());
 		SmartDashboard::PutNumber(" Right Side Encoder Count: ",      crvbot.rightEnc->GetRaw());
+		SmartDashboard::PutNumber(" Left Side Rate :", 				  crvbot.leftEnc->GetRate());
+		SmartDashboard::PutNumber(" RightSide Rate :" ,               crvbot.rightEnc->GetRate());
 		SmartDashboard::PutNumber(" FuelShooter Encoder Position: ",  crvbot.fuelManipulator->GetEncPosition());
 		SmartDashboard::PutNumber(" FuleShooter RPM: ",      		  crvbot.fuelManipulator->GetEncVel());
-		SmartDashboard::PutNumber("Gyro Angle", 					  crvbot.gyro->GetAngle());
+		//SmartDashboard::PutNumber(" RobotCenter X Value: ",           vs.alinementToBoiler());
+		SmartDashboard::PutNumber(" Gyro Angle : ", 				  crvbot.gyro->GetAngle());
 	}
 
 	void updateRobotPreference(void) {
 
-		speedShoot   = prefs->GetDouble("Shooter Speed Scale", 0.4);
-		tankTrue     = prefs->GetBoolean("Is tankDrive on?", true);
-		AutonChooser = prefs->GetInt("Choose Auton", 9);
-		crvbot.kP	 = prefs->GetDouble("P", 0.0);
-		crvbot.kI	 = prefs->GetDouble("I", 0.0);
-		crvbot.kD	 = prefs->GetDouble("D", 0.0);
+
+		reverseDirection  = prefs->GetBoolean("Is tankDrive on?", true);
+		BoilerPostition   = prefs->GetInt("BoilerPostition ", 300);
+		AutonChooser      = prefs->GetInt("Choose Auton", 9);
+		crvbot.kP	      = prefs->GetDouble("P", 0.0);
+		crvbot.kI	      = prefs->GetDouble("I", 0.0);
+		crvbot.kD	 	  = prefs->GetDouble("D", 0.0);
 	}
-
-
 
 };
 
