791e5fb245d441c4585f17004e4a331cbc893217
Martin Waby Smoger <mvsmoger@gmail.com>
2017-03-13 22:36:50 -0400

    Reconfig Talon SRXs
    Renamed fuelManipulator to fuelShooter.
    Changed that both shooter Talons are controlled separately rather than
    master slave config. 
    
    Added Voltage ramp ups to each Talon SRX.

--- CrevoLib/src/CrevoRobot.h
+++ CrevoLib/src/CrevoRobot.h
@@ -115,8 +115,8 @@ public:
 		 CANTalon 	  *leftFrontMotor	 = NULL;
 		 CANTalon 	  *leftRearMotor	 = NULL;
 		 CANTalon 	  *intakeRoller 	 = NULL;
-		 CANTalon 	  *fuelShooter   = NULL;
-		 CANTalon 	  *fuelShooter2  = NULL;
+		 CANTalon 	  *fuelShooter1      = NULL;
+		 CANTalon 	  *fuelShooter2      = NULL;
 		 CANTalon	  *agitatorMotor     = NULL;
 		 CANTalon	  *hangerMotor 	     = NULL;
 
